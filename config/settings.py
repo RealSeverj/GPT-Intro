7fbca8b4abec6957a31ddec3b0a8b0345af36fbf
Severj <Severj@qq.com>
2025-02-15 23:32:16 +0800

    中文初尝试

--- config/settings.py
+++ config/settings.py
@@ -1,19 +1,19 @@
 # 模型参数
 MODEL_CONFIG = {
-    "vocab_size": 16,
-    "d_model": 16,
-    "nhead": 1,
-    "num_layers": 1,
-    "block_size": 16
+    "vocab_size": 66000,
+    "d_model": 256,
+    "nhead": 2,
+    "num_layers": 2,
+    "block_size": 64
 }
 
 # 训练参数
 TRAIN_CONFIG = {
-    "batch_size": 1,
+    "batch_size": 32,
     "base_epochs": 1,
     "learning_rate": 1e-4,
-    "max_seq_len": 16,
-    "max_iters": 10,
-    "lr_decay_iters": 10,
-    "dropout": 0.0
+    "max_seq_len": 256,
+    "max_iters": 500,
+    "lr_decay_iters": 500,
+    "dropout": 0.1
 }
